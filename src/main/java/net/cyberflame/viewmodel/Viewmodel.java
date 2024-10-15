ae81bd00d06d754ea33359eeda10c468e1a8f395
CyberFlame <cyberflameu@gmail.com>
2024-10-15 18:43:52 +1300

    Add keybind configuration (#176)
    * add keybind api
    
    Signed-off-by: CyberFlame <cyberflameu@gmail.com>
    
    * implement feature
    
    Signed-off-by: Aaron Lowe <cyberflameu@gmail.com>
    
    * add translations, bump version
    
    Signed-off-by: Aaron Lowe <cyberflameu@gmail.com>
    
    ---------
    
    Signed-off-by: CyberFlame <cyberflameu@gmail.com>
    Signed-off-by: Aaron Lowe <cyberflameu@gmail.com>

--- src/main/java/net/cyberflame/viewmodel/Viewmodel.java
+++ src/main/java/net/cyberflame/viewmodel/Viewmodel.java
@@ -2,14 +2,20 @@ package net.cyberflame.viewmodel;
 
 import net.cyberflame.viewmodel.config.LoadConfig;
 import net.cyberflame.viewmodel.config.SaveConfig;
+import net.cyberflame.viewmodel.gui.ViewmodelScreen;
 import net.cyberflame.viewmodel.settings.Setting;
 import net.cyberflame.viewmodel.settings.SettingType;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
+import org.lwjgl.glfw.GLFW;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -25,6 +31,7 @@ public class Viewmodel implements ModInitializer {
     private static final List<Setting<?>> SETTINGS = Arrays.stream(SettingType.values())
             .map(SettingType::getSetting)
             .collect(Collectors.toList());
+    private static KeyBinding keyBinding;
 
     @Contract(value = " -> new", pure = true)
     public static @NotNull List<Setting<?>> getSettings() {
@@ -45,6 +52,16 @@ public class Viewmodel implements ModInitializer {
         } catch (IOException e) {
             LOGGER.error("Failed to load settings!", e);
         }
+        keyBinding = KeyBindingHelper.registerKeyBinding(
+                new KeyBinding("key.viewmodel.open", InputUtil.Type.KEYSYM,
+                        GLFW.GLFW_KEY_BACKSLASH, "category.viewmodel.menu"));
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (keyBinding.wasPressed()) {
+                if (client.world != null) {
+                    client.setScreen(new ViewmodelScreen());
+                }
+            }
+        });
         getRuntime().addShutdownHook(new Thread(SaveConfig::saveAllSettings));
     }
 
